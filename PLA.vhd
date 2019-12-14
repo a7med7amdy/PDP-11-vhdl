09224c60bfdaaca6a4c82e3749ecca1385f9ed79
MhmdAbdlh <47702978+MhmdAbdlh@users.noreply.github.com>
2019-12-14 03:50:18 +0200

    Add PLA and fix some stuff

--- PLA.vhd
+++ PLA.vhd
@@ -1,2 +1,88 @@
-LIBRARY IEEE;
-USE IEEE.std_logic_1164.all;
+library ieee;
+use ieee.std_logic_1164.all;
+
+ENTITY PLA IS
+	PORT (
+	IR : IN  std_logic_vector (15 Downto 0);
+	M0 : IN  std_logic_vector (2 Downto 0);
+	BR,HLT,OneOP,OneORTwoOP, RegisterDirect : OUT std_logic;
+	Address : OUT std_logic_vector(4 Downto 0));
+END ENTITY PLA;
+
+ARCHITECTURE struct OF PLA IS
+
+signal OP1,OP2, Jmp, RTS, ZeroOP,RegDirect, op1or2 : std_logic;
+
+begin
+	OP2       <= not(IR(15)) or ( IR(15) and not(IR(14)) and not(IR(13)) and not(IR(12)) );
+	OP1       <= IR(15) and not(IR(14)) and IR(13);
+	op1or2    <= OP1 or OP2;
+	BR        <= IR(15) and IR(14) and not(IR(13) and IR(12) and IR(11)) ;
+	ZeroOP    <= IR(15) and IR(14) and IR(13) and IR(12) and IR(11) ;
+	RegDirect <= not (M0(2) or M0(1) or M0(0)) and op1or2;
+	HLT       <= ZeroOP and not(IR(10)) and not(IR(9)) and not(IR(8));
+	Jmp       <= ZeroOP and not(IR(10)) and IR(9) and not(IR(8));
+	RTS       <= ZeroOP and not(IR(10)) and IR(9) and IR(8);
+
+	Address(0) <= RTS;
+	Address(1) <= RegDirect;
+	Address(2) <= op1or2 or RTS;
+	Address(3) <= Jmp or RTS;
+	Address(4) <= RegDirect or jmp or RTS;
+	
+	RegisterDirect<=RegDirect;
+	OneOP      <= OP1;
+	OneORTwoOP <= op1or2;
+end struct;
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+ENTITY mux4_2bit IS
+PORT( in0,in1,in2,in3: IN std_logic_vector (1 DOWNTO 0);
+              sel0,sel1 : IN std_logic;
+              out1: OUT std_logic_vector (1 DOWNTO 0));
+END mux4_2bit;
+
+
+ARCHITECTURE  arch1 OF mux4_2bit IS
+BEGIN
+        out1 <=   in0 WHEN sel0 = '0' AND sel1 ='0'
+            ELSE in1 WHEN   sel0 = '0' AND sel1 ='1'
+            ELSE in2 WHEN   sel0 = '1' AND sel1 ='0'
+            ELSE in3;
+
+END arch1;
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+ENTITY LoadFromPLA IS
+	PORT (
+	IR : IN std_logic_vector (15 Downto 0);
+	M0 : IN std_logic_vector (2 Downto 0);
+	S_bit:  IN std_logic ;
+	BR,HLT,OneOP : OUT std_logic;
+	Address : OUT std_logic_vector(4 Downto 0));
+END ENTITY LoadFromPLA;
+
+Architecture struct2 of  LoadFromPLA is
+
+signal OneORTwoOP,RegisterDirect : std_logic;
+signal P : std_logic_vector(4 downto 0);
+signal MUXout,Mux0,p1Sbit: std_logic_vector(1 downto 0);
+
+
+begin
+	Mux0 <= M0(1 downto 0);
+	p1Sbit <= P(1)&S_bit;
+	L0: entity work.PLA port map (IR,M0,BR,HLT,OneOP,OneORTwoOP,RegisterDirect,P);
+	L1: entity work.mux4_2bit port map (P(1 downto 0),P(1 downto 0),Mux0,p1Sbit,OneORTwoOP,RegisterDirect,MUXout);
+
+	Address <= P(4 downto 2) & (P(1) or MUXout(1)) & (P(0) or MUXout(0));
+
+end struct2;
+
+
+
--- loadFromNextAddress.vhd
+++ loadFromNextAddress.vhd
@@ -10,17 +10,20 @@ entity loadFromNextAddress is
         AddOut   : OUT std_logic_vector(4 DOWNTO 0));
 end loadFromNextAddress;
 
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.all;
+
 architecture arch of loadFromNextAddress is
     COMPONENT mux4 IS
     PORT( in0,in1,in2,in3: IN std_logic;
     sel : IN std_logic_vector (1 DOWNTO 0);
     out1: OUT std_logic);
-    END COMPONENT ;
+    END COMPONENT;
 -------------------------------------------------------------------
     COMPONENT mux2 IS
     PORT (IN1,IN2,SEl	:  IN std_logic;
             OUT1        : OUT  std_logic); 
-    END COMPONENT ;
+    END COMPONENT; 
 -------------------------------------------------------------------
 COMPONENT mux2_3bit IS
 PORT (IN1,IN2   :  IN std_logic_vector(2 DOWNTO 0);
@@ -30,16 +33,15 @@ END COMPONENT ;
 -------------------------------------------------------------------
     signal  muxout,auxNor : std_logic;
     signal  N0new,N0new2 : std_logic;
-    signal  selAux1,selAux2 : std_logic;
+    signal  selAux : std_logic;
 
 begin
 auxNor <= not(M(0) or M(1) or M(2));
 u0: mux4 PORT MAP (NextAddressField(0),Sbit,auxNor,M(2),F6,muxout);
-N0new <= N0new or muxout;
+N0new <= NextAddressField(0) or muxout;
 N0new2 <= N0new or Sbit;
-selAux1 <= F6(0) or F6(1) or NextAddressField(0);
-selAux2 <= F6(0) or F6(1) or not(NextAddressField(0));
-u1: mux2 PORT MAP (N0new,N0new2,selAux2,AddOut(0));
-u2: mux2_3bit PORT MAP ("000",NextAddressField(3 DOWNTO 1),selAux1,AddOut(3 DOWNTO 1));
+selAux <= F6(0) and F6(1) and N0new;
+u1: mux2 PORT MAP (N0new,N0new2,selAux,AddOut(0));
+u2: mux2_3bit PORT MAP (NextAddressField(3 DOWNTO 1),"000",selAux,AddOut(3 DOWNTO 1));
 AddOut(4) <= NextAddressField(4);
 end arch ; -- arch
