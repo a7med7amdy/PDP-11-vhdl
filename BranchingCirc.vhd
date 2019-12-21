6ece90ed5a474425d0caf87d704528d50331c034
[sha3er97] <37841787+sha3er97@users.noreply.github.com>
2019-12-21 22:57:17 +0200

    make offset signed vector

--- BranchingCirc.vhd
+++ BranchingCirc.vhd
@@ -1,5 +1,6 @@
-LIBRARY IEEE;
-USE IEEE.std_logic_1164.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 Entity BranchingCirc is
 	port(PCin,IR,FlagReg : IN std_logic_vector(15 Downto 0);
@@ -7,22 +8,26 @@ Entity BranchingCirc is
 end BranchingCirc;
 
 architecture BranchingCircArch of BranchingCirc is
-    component adder_bits_16 is
-        port(
-            a,b : IN std_logic_vector(15 DOWNTO 0);
-            Cin : IN std_logic;
-            S   : OUT std_logic_vector(15 DOWNTO 0);
-            Cout : OUT std_logic);
+    component Adder_bits_n IS    
+    PORT (    
+        a, b  :  IN  std_logic_vector(15 downto 0);
+        Ci   :  IN  std_logic; 
+        S     :  OUT std_logic_vector(15 downto 0);
+        Co  :  OUT std_logic;
+        V     :  OUT std_logic  );  
     end component;
+---------------------------------------------------------------
     component mux2_16bit IS  
       PORT (IN1,IN2  :  IN std_logic_vector(15 DOWNTO 0);
             SEl	:  IN std_logic;
           OUT1        : OUT  std_logic_vector(15 DOWNTO 0)); 
     end component;
-  
-    signal Carry,Zero,condition,uselessC : std_logic;
+---------------------------------------------------------------
+    signal Carry,Zero,condition,uselessC,uselessV : std_logic;
     signal selector : std_logic_vector(2 Downto 0);
-    signal offset,OP2 : std_logic_vector(15 Downto 0);
+    signal helpWithSign : std_logic_vector(8 Downto 0); -- 9 bits
+    --signal offset : signed(15 downto 0);
+    signal OP2,offset : std_logic_vector(15 Downto 0);
 begin
 	process(IR,FlagReg)
 	begin
@@ -45,9 +50,10 @@ begin
         condition<= Carry or Zero;
     end if ;
     end process;
-    
-    offset <= "0000000"&IR(7 Downto 0)&'0';
-    ADD: adder_bits_16 PORT MAP(offset,PCin,'0',OP2,uselessC);
+    helpWithSign <= (others => IR(7));
+    offset <= helpWithSign(8 Downto 0) & IR(6 Downto 0); --extend with sign
+    ADD: Adder_bits_n PORT MAP(offset,PCin,'0',OP2,uselessC,uselessV);
+    --OP2 <= signed(offset) + unsigned(PCin);
     M: mux2_16bit PORT MAP (Pcin,OP2,condition,PCout);
 
 end BranchingCircArch;
