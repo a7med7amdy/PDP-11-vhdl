320ff63d3f5c0c780bc46510f835e7ec0aa881ac
MhmdAbdlh <47702978+MhmdAbdlh@users.noreply.github.com>
2019-12-26 07:52:34 +0200

    ...

--- reg16.vhd
+++ reg16.vhd
@@ -72,3 +72,52 @@ ELSif d /="ZZZZZZZZZZZZZZZZ" then
 END IF;
 END PROCESS;
 END arch;
+
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.all;
+
+entity regFR is
+    port(
+        d : IN std_logic_vector(15 DOWNTO 0);
+        Clk,Rst : IN std_logic;
+        q   : OUT std_logic_vector(15 DOWNTO 0));
+end regFR;
+
+
+
+
+ARCHITECTURE arch OF regFR IS
+BEGIN
+PROCESS (Clk,Rst)
+BEGIN
+IF Rst = '1' THEN
+		q <= (OTHERS=>'0');
+ELSIF rising_edge(Clk) THEN
+		q<=d;
+END IF;
+END PROCESS;
+END arch;
+
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.all;
+
+entity regPC is
+    port(
+        d,brd : IN std_logic_vector(15 DOWNTO 0);
+        Clk,Rst,br : IN std_logic;
+        q   : OUT std_logic_vector(15 DOWNTO 0));
+end regPC;
+
+ARCHITECTURE arch OF regPC IS
+BEGIN
+PROCESS (d,Rst,br)
+BEGIN
+IF Rst = '1' THEN
+		q <= (OTHERS=>'0');
+elsif br = '1' then
+	q<=brd;
+ELSif d /="ZZZZZZZZZZZZZZZZ" then
+	q <= d;
+END IF;
+END PROCESS;
+END arch;
